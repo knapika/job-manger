f2152d9bebf14909c7e9d424d611b783c6f51854
Andrzej <andrzej.knapik1@gmail.com>
2018-10-25 19:42:38 +0200

    Init commit

---
+++ java-server/src/main/java/com/example/javaserver/JavaServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.javaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaServerApplication.class, args);
+	}
+}
