82db9a5411afc44aecd86ef3ebe8d120cfd2260e
Andrzej <andrzej.knapik1@gmail.com>
2018-12-22 18:22:31 +0100

    Routing added & styles changed

--- webclient/src/app/app.module.ts
+++ webclient/src/app/app.module.ts
@@ -2,27 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { OffersListComponent } from './offers-list/offers-list.component';
+
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { OffersListComponent } from './offers/offers-list/offers-list.component';
+import { OffersModule } from './offers/offers.module';
 
-
+const appRoutes: Routes = [
+  { path: 'offers', component: OffersListComponent },
+];
 
 @NgModule({
+  
   declarations: [
     AppComponent,
-    OffersListComponent,
-    MainNavComponent,
     
   ],
   imports: [
+    OffersModule,
+    RouterModule.forRoot(
+      appRoutes,
+    ),
+
     BrowserModule,
     BrowserAnimationsModule,
-    AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
---
+++ webclient/src/app/offers/offers.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { OffersListComponent } from './offers-list/offers-list.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    
+  ],
+  exports: [],
+  declarations: [OffersListComponent]
+})
+export class OffersModule { }
